40ad3777b84cb5ac32bbcd04706b03f2d76c467f
WYChoi1995 <60026903+WYChoi1995@users.noreply.github.com>
2024-08-22 10:38:03 +0900

    added BOK reader

--- econdatareader/tests/test_finance.py
+++
@@ -1,4 +0,0 @@
-from econdatareader.finance import FinanceDownloader
-
-downloader = FinanceDownloader()
-downloader.download_data('CRYPTO_SPOT_BINANCE', ['BTCUSDT'], '1m', 202407240000, 202408050000)
