0ac95bacd9b7e2f0d42247b8ab440d3c08d40124
WYChoi1995 <60026903+WYChoi1995@users.noreply.github.com>
2024-08-24 12:36:58 +0900

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open('readme.md', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='econ-datareader',
     version='0.2.2',
     description='Download Econ Data - Macro and Finance',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='WyChoi1995',
     author_email='wydanielchoi@gmail.com',
     url='https://github.com/WYChoi1995/econdatareader',
