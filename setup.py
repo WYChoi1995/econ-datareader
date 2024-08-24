1634a0be56dd26ec08755cf3cea735cfbf1a5108
WYChoi1995 <60026903+WYChoi1995@users.noreply.github.com>
2024-08-24 11:27:14 +0900

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='econ-datareader',
-    version='0.2.1',
+    version='0.2.2',
     description='Download Econ Data - Macro and Finance',
     author='WyChoi1995',
     author_email='wydanielchoi@gmail.com',
